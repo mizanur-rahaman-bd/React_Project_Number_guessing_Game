f3b039cceebf9e756fa8d614f154042f6fb3d111
mizanur-rahaman-bd <sumon1581@gmail.com>
2025-03-15 21:14:41 +0600

    second

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="container">
+        <h1 className="heading bg-[#000]">fdhfdh</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
